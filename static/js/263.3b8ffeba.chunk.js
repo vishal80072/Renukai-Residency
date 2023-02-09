dde98ae04f4d02e2f53a675a27f718c104d8841f
Vishal Surgade <vishalsurgade4@gmail.com>
2023-02-09 15:20:26 +0530

    Bug fixes

--- static/js/263.3b8ffeba.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_vishalsurgade_renukai_residency_jalgaon=self.webpackChunk_vishalsurgade_renukai_residency_jalgaon||[]).push([[263],{55263:function(e,a,n){n.r(a);var r=n(80184);a.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"About us page"})})}}}]);
-//# sourceMappingURL=263.3b8ffeba.chunk.js.map
