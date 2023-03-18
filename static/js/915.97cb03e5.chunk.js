0c2bcf7434480e1950ca481588a99f72f45fbe5e
Vishal Surgade <vishalsurgade4@gmail.com>
2023-03-18 14:35:07 +0530

    Completed

--- static/js/915.97cb03e5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_vishalsurgade_renukai_residency_jalgaon=self.webpackChunk_vishalsurgade_renukai_residency_jalgaon||[]).push([[915],{61915:function(e,a,n){n.r(a);var r=n(80184);a.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"Dashboard"})})}}}]);
-//# sourceMappingURL=915.97cb03e5.chunk.js.map
